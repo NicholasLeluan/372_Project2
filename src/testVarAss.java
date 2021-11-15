78865ddbbbe961a9669f18bcf9bd6e3501a55582
nicholasleluan <nicholasleluan@email.arizona.edu>
2021-11-15 11:36:06 -0700

    added negative numbers to logic

--- src/testVarAss.java
+++ src/testVarAss.java
@@ -1,11 +1,12 @@
-public class Test3 {
+public class testVarAss {
 	public static void main(String[] args){
-double x = 3 + 4.1;
+double x = 3 + -4.1;
 double c = 5 - 4.1;
-double y = 3 * 6.1;
+int y = -3 * 6;
 double p = 3 % 3.1;
 double b = 3 * 3.1;
-double a = 3 * 3.1;
+int a = 3 * -3;
 boolean s = true;
-s = false;	}
+s = false;
+}
 }

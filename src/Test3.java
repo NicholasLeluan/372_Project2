7b7786c62d60c73910540c5686dfdcf92e0b1609
nicholasleluan <nicholasleluan@email.arizona.edu>
2021-11-14 18:37:16 -0700

    refactored the variable assignment; now works for new and existing variables. Added header for if statements; robust conditional statements

--- src/Test3.java
+++ src/Test3.java
@@ -1,4 +1,11 @@
 public class Test3 {
 	public static void main(String[] args){
-	}
+double x = 3 + 4.1;
+double c = 5 - 4.1;
+double y = 3 * 6.1;
+double p = 3 % 3.1;
+double b = 3 * 3.1;
+double a = 3 * 3.1;
+boolean s = true;
+s = false;	}
 }
